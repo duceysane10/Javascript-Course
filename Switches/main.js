bceef0d3ef17ede3ba1039d62216e52be018cf1d
Abdifitah Yusuf Mohamud <engduceysane10@gmail.com>
2023-01-08 11:07:30 +0300

    i added switch lesson

---
+++ Switches/main.js
@@ -0,0 +1,23 @@
+/*  ***********************
+Switch :
+waa sida if balse la is ticmaalaayo case
+**********************************/
+
+let exp ="cambe";
+
+switch(exp){
+    case "tufax":
+    console.log("tufaaxa waa $.5");
+    break;
+    case "cambe":
+        console.log("cambe waa $1");
+        break;
+    case "saytuun":
+        console.log("saytuun waa $1.5");
+        break;
+    default:
+        console.log("lama haayo Qudaartaas");
+}
+
+
+
