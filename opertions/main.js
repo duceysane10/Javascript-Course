f6b25f74ed1112e8f0df17d7ae36f52dbc0ea410
Abdifitah Yusuf Mohamud <engduceysane10@gmail.com>
2023-01-08 10:25:42 +0300

    finshed operations

--- opertions/main.js
+++ opertions/main.js
@@ -1,10 +1,70 @@
+/*  ***********************
+1. Arithmatic Opertions
+**********************************/
+
+
 // Opertionska Waxaa ka mid "+", "-", "*", "/", "%"
 //eaxamples
+// "=" calaamadaas waxaa la yiraa asignment opertor varibaleka ayeey qiimaha ku shubtaa
 
-let x = 25;
-var y = 6;
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y); // waxay soo celinee haraaga 
+// let x = 25;
+// var y = 6;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y); // waxay soo celinee haraaga 
+
+
+
+
+/*  ***********************
+2. Comparison Operator is bar bardhigid 
+**********************************/
+
+
+/*
+"== " equal
+"!= " not equal
+"=== " strict equal: waxaa la is ticmaalaa marki la hubinaayo inay isku mid ka yihiin datada iyo data type keeda labaduba
+"!==" strict not equal
+">" greater than
+"<" less than
+">=" gretaer than or equal
+"<=" less than or equal
+
+*/ 
+
+// Examples
+
+// let var1 = 10;
+// let var2 = "duco";
+// let var3 = 6;
+
+// console.log(var1 == var2);
+// console.log(var1 != var2);
+// console.log(var1 === var3);
+// console.log(var1 < var3);
+// console.log(var1 > var3);
+// console.log(var1 !== var2);
+
+
+
+
+
+/*  ***********************
+3. Logical Opertions 
+****************************/
+// AND (&&) both must be true 
+//OR (||) one of them is true
+// Not (!) waa wixii la sameeyo cagsigeeda
+//Examples
+
+let x = 10;
+let y= 15;
+let z = 4;
+let var5 = false || true;
+let var2 = !true
+console.log(x && y < z)
+console.log(var5)
+console.log(var2)
