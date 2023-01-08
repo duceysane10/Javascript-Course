3c87c53a22c957488a5c2ee0bae51d265a6ede25
Abdifitah Yusuf Mohamud <engduceysane10@gmail.com>
2023-01-08 10:40:36 +0300

    i added Control flow lesson

---
+++ Control flow/main.js
@@ -0,0 +1,29 @@
+/*  ***********************
+1. Control Flow :
+waa inaan Controlnaa meesha codekeena maraayo iyo sida loo kala hor marsiinaayo
+**********************************/
+//if , ifelse,else
+
+let per = 70;
+
+if(per > 90){
+    console.log("A+");
+}
+else if(per > 80 ){
+    console.log("A");
+}
+else if(per > 70 ){
+    console.log("B+");
+}
+else if(per > 60 ){
+    console.log("B");
+}
+else if(per > 55 ){
+    console.log("C");
+}
+else if(per > 50 ){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
