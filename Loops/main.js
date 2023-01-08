a7647a999afe9c6e8d0913c82235f82246581594
Abdifitah Yusuf Mohamud <engduceysane10@gmail.com>
2023-01-08 11:36:42 +0300

    i added loops lesson

---
+++ Loops/main.js
@@ -0,0 +1,42 @@
+
+
+/*  ***********************
+ Loops :
+waa in xogta dhowr jeer oo is xig xigta lasoo cel celsho
+**********************************/
+//Types of loop are: Forloop, while loop, do while loop, foreach loop
+
+// loopku wuxuu ubaahan yahay sadax shey meel uu ka bilaawdo , meel uu ku dhamaado iyo intaas cida gaarsiisa
+// 1 For loop
+
+// let per = "Java script";
+// for(let i=0; i < 10; i++){
+//     console.log(per);
+// }
+//  upove code will print java script 10 times
+
+
+// 2 while loop:
+
+// let per = "Java script";
+// let i =0;
+// while(i < 10){
+//     console.log(per); 
+//     i++;
+// }
+
+
+// 3 do while  Loop
+let i = 0;
+let per = "Java script";
+do {
+    console.log(per); 
+    i++;
+}while(i < 10);
+
+
+/// infint loop : waa loop aan dhamaad laheen ama la cayimin meeluu ku dhaamn lahaa
+// exmple
+while(true){
+    console.log('infint loop');
+}
