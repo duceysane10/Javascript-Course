f7fb06d1a4622a4e366a35dddbe0b81c92d7dc17
Abdifitah Yusuf Mohamud <engduceysane10@gmail.com>
2023-01-08 09:20:09 +0300

    variables

---
+++ variables/main.js
@@ -0,0 +1,12 @@
+var a =5;
+let b =8;
+console.log(a+b);
+
+/// varible const ayaa jiro oo hadii mar hadii qiimo loo asign gareeyp aan dib looga badali karin
+
+// tusaale
+
+const year = 2022;
+ let age = 1995;
+
+ console.log( year - age);
